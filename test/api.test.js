a60c2f719265d12bfd85e41ab2f19fbdcb01509f
Sahil Gothoskar <33109078+SahilGothoskar@users.noreply.github.com>
2023-02-13 10:26:42 -0500

    Update api.test.js

--- test/api.test.js
+++ test/api.test.js
@@ -12,8 +12,8 @@ chai.use(chaiHTTP);
 var assert = require('assert');
 describe('Array', function () {
   describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal([1, 2, 3].indexOf(6), -1);
+   // it('should return -1 when the value is not present', function () {
+     // assert.equal([1, 2, 3].indexOf(6), -1);
     });
   });
 });
